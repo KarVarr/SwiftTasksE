ee92ff0b4b1ed24191f1cc2a3754a379e4d99b8b
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2025-05-14 14:30:23 +0400

    Update Task 1: Palindrome.swift

--- Task 1: Palindrome.swift
+++ Task 1: Palindrome.swift
@@ -1,1 +1,6 @@
+public func isPalindrome(input: String) -> Bool {
+  guard input.count > 1 else {return false}
+  let newInput = input.lowercased().filter {$0.isLetter || $0.isNumber}
+  return newInput == String(newInput.reversed())
+}
 
