6acb2132a0ba9cc46c8b9ad77ce7ae70e5da22a0
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2025-05-14 16:15:58 +0400

    Update Task 2: BalancedParentheses.swift

--- Task 2: BalancedParentheses.swift
+++ Task 2: BalancedParentheses.swift
@@ -1,1 +1,16 @@
-
+public func isBalancedParentheses(input: String) -> Bool {
+    var arr = [Character]()
+    
+    for char in input {
+        if char == "(" {
+            arr.append(char)
+        } else if char == ")" {
+            if arr.isEmpty {
+                return false
+            }
+            arr.removeLast()
+        }
+    }
+    
+    return arr.isEmpty 
+}
