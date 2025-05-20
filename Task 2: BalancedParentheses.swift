b41f836b40001fae4d24e44d521ee73ebd3c0971
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2025-05-20 17:39:09 +0400

    Update Task 2: BalancedParentheses.swift
    Added test cases: ")(" and )()("

--- Task 2: BalancedParentheses.swift
+++ Task 2: BalancedParentheses.swift
@@ -1,6 +1,5 @@
 public func isBalancedParentheses(input: String) -> Bool {
     var arr = [Character]()
-    
     for char in input {
         if char == "(" {
             arr.append(char)
@@ -11,6 +10,13 @@ public func isBalancedParentheses(input: String) -> Bool {
             arr.removeLast()
         }
     }
-    
     return arr.isEmpty 
 }
+
+//test
+print(isBalancedParentheses(input: ")("))// false
+print(isBalancedParentheses(input: ")()(")) // false
+print(isBalancedParentheses(input: "(()())"))   // true
+print(isBalancedParentheses(input: "test(letters)karen()"))// true
+print(isBalancedParentheses(input: "(())")) // true
+print(isBalancedParentheses(input: "(()"))  // false
