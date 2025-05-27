648eaf4e94ddac13d798e0224ff6628940d163e1
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2025-05-27 11:39:56 +0400

    Update Task 1: Library Management System.swift

--- Task 1: Library Management System.swift
+++ Task 1: Library Management System.swift
@@ -1,1 +1,66 @@
+protocol Borrowable {
+    var borrowDate: Date? {get set}
+    var returnDate: Date? {get set}
+    var isBorrowed: Bool {get set}
+    mutating func checkIn()
+}
 
+extension Borrowable {
+    func isOverdue() -> Bool {
+        guard let returnDate = returnDate else {return false}
+        return Date() > returnDate
+    }
+    mutating func checkIn() {
+        borrowDate = nil
+        returnDate = nil
+        isBorrowed = false
+    }
+}
+
+class Item {
+    let id: String
+    let title: String
+    let author: String
+
+    init(id: String, title: String, author: String) {
+        self.id = id
+        self.title = title
+        self.author = author
+    }
+}
+
+class Book: Item, Borrowable {
+    var borrowDate: Date?
+    var returnDate: Date?
+    var isBorrowed: Bool = false
+}
+
+enum LibraryError: Error {
+    case itemNotFound
+    case itemNotBorrowable
+    case alreadyBorrowed
+}
+
+class Library {
+    var items: [String: Item] = [:]
+    func addBook(_ book: Book) {
+        items[book.id] = book
+    }
+
+    func borrowItem(by id: String) throws -> Item {
+        guard let item = items[id] else {
+            throw LibraryError.itemNotFound
+        }
+        guard let book = item as? Book else {
+            throw LibraryError.itemNotBorrowable
+        }
+        if book.isBorrowed {
+            throw LibraryError.alreadyBorrowed
+        }
+        
+        book.borrowDate = Date()
+        book.returnDate = Calendar.current.date(byAdding: .day, value: 5, to: Date())
+        book.isBorrowed = true
+        return book
+    }
+}
