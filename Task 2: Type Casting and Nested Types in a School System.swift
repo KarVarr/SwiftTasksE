623ada6f88b77fbf95ed2406b67aeccfbeb41ab3
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2025-05-27 12:09:37 +0400

    Update Task 2: Type Casting and Nested Types in a School System.swift

--- Task 2: Type Casting and Nested Types in a School System.swift
+++ Task 2: Type Casting and Nested Types in a School System.swift
@@ -1,1 +1,36 @@
+struct School {
+    enum SchoolRole {
+        case student
+        case teacher
+        case administrator
+    }
+    
+    class Person {
+        let name: String
+        let role: SchoolRole
+        
+        init(name: String, role: SchoolRole) {
+            self.name = name
+            self.role = role
+        }
+    }
+    var person: [Person] = []
+    mutating func addPerson(_ newPerson: Person) {
+        person.append(newPerson)
+    }
+    subscript(role: SchoolRole) -> [Person] {
+        return person.filter {$0.role == role}
+    }
+}
 
+func countStudents(in school: School) -> Int {
+    return school[.student].count
+}
+
+func countTeachers(in school: School) -> Int {
+    return school[.teacher].count
+}
+
+func countAdministrators(in school: School) -> Int {
+    return school[.administrator].count
+}
